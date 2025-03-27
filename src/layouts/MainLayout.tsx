e0cd15c1d4e0665bc3df3fdfb0dcd12fa1e4a748
sorawat38 <st.sorawat38@gmail.com>
2025-03-27 16:58:17 -0600

    [build] move navbar and footer to organisms

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer.tsx'
-import Navbar from '../components/Navbar.tsx'
+import Footer from '../components/organisms/Footer.tsx'
+import Navbar from '../components/organisms/Navbar.tsx'
 
 const MainLayout = () => {
   return (
