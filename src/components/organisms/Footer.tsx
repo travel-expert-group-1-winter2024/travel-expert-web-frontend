1d614236cee672394b9a47220f1d64e82315d149
sorawat38 <st.sorawat38@gmail.com>
2025-03-29 12:09:39 -0600

    [feat] create simple footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <div>This is footer</div>
+  return (
+    <footer className='bg-primary text-primary-foreground py-6'>
+      <div className='container mx-auto text-center'>
+        <p>
+          &copy; {new Date().getFullYear()} Travel Experts. All rights reserved.
+        </p>
+        <nav className='mt-2'>
+          <a href='#' className='text-muted-foreground hover:text-white'>
+            Privacy Policy
+          </a>{' '}
+          |
+          <a href='#' className='text-muted-foreground ml-2 hover:text-white'>
+            Terms of Service
+          </a>{' '}
+        </nav>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
