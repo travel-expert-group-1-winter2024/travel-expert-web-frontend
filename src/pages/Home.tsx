4ef943d83822a32cfe21f0ac05dae6c569c95e9f
sorawat38 <st.sorawat38@gmail.com>
2025-03-28 21:57:27 -0600

    [feat] add travel perks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/organisms/Hero.tsx'
+import TravelPerks from '@/components/organisms/TravelPerks.tsx'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <TravelPerks />
     </>
   )
 }
