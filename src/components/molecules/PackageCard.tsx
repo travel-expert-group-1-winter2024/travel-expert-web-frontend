c913ff9cdbcb49fc91cb46b1cbcbf37f70e9a52f
sorawat38 <st.sorawat38@gmail.com>
2025-04-07 22:20:03 -0600

    [feat] create PackageCard

---
+++ src/components/molecules/PackageCard.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button.tsx'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card.tsx'
+import { Package } from '@/types/package.ts'
+
+type Props = {
+  pkg: Package
+  imageUrl?: string
+}
+
+export function PackageCard({ pkg, imageUrl }: Props) {
+  return (
+    <Card>
+      <CardHeader className='min-h-15'>
+        <CardTitle>{pkg.pkgname}</CardTitle>
+        <CardDescription>{pkg.pkgdesc}</CardDescription>
+      </CardHeader>
+      <CardContent className='flex aspect-square items-center justify-center p-6'>
+        <img
+          src={imageUrl || 'https://placehold.co/600x400'}
+          alt={pkg.pkgname}
+          className='h-full w-full object-cover'
+        />
+      </CardContent>
+      <CardFooter className='flex justify-between'>
+        <p>
+          {(pkg.pkgbaseprice + pkg.pkgagencycommission).toLocaleString(
+            'en-CA',
+            {
+              style: 'currency',
+              currency: 'CAD',
+            },
+          )}
+        </p>
+        <Button>Book</Button>
+      </CardFooter>
+    </Card>
+  )
+}
