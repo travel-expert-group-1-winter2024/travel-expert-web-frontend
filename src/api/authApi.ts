3be9c22583cb1dde61c5e0170fc9c7be2a8901d0
sorawat38 <st.sorawat38@gmail.com>
2025-04-09 10:29:07 -0600

    [feat] support refresh page

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,6 +1,15 @@
 import api from '@/api/axios.ts'
 import { LoginRequest } from '@/types/auth.ts'
+import { GeneralApiResponse } from '@/types/generalApiResponse.ts'
 import { LoginResponse } from '@/types/loginResponse.ts'
+import { User } from '@/types/userInfo.ts'
 
 export const userLogin = (data: LoginRequest) =>
   api.post<LoginResponse>('/api/login', data)
+
+export const authUser = (token: string) =>
+  api.get<GeneralApiResponse<User>>('/api/auth/me', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,9 +1,9 @@
-import { userLogin } from '@/api/authApi.ts'
+import { authUser, userLogin } from '@/api/authApi.ts'
 import { LoginRequest } from '@/types/auth.ts'
-import { User } from '@/types/loginResponse.ts'
+import { User } from '@/types/userInfo.ts'
 import { useMutation } from '@tanstack/react-query'
 import * as React from 'react'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 interface AuthContextType {
@@ -31,8 +31,6 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
   const mutation = useMutation({
     mutationFn: userLogin,
   })
-
-  const isLoggedIn = !!token
 
   const loginAction = (
     data: LoginRequest,
@@ -62,6 +60,29 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
     localStorage.removeItem('site')
     navigate('/login')
   }
+
+  // add useEffect here to prevent user from being logged out when the page is refreshed
+  useEffect(() => {
+    // check if token is present in local storage
+    const storedToken = localStorage.getItem('site')
+    if (!storedToken) return
+
+    // if token is present, set it to state by
+    const restoreUser = async () => {
+      try {
+        const res = await authUser(token)
+        setToken(token)
+        setUser(res.data.data)
+      } catch (err) {
+        console.error('Session restore failed:', err)
+        logOut()
+      }
+    }
+
+    restoreUser()
+  }, [])
+
+  const isLoggedIn = !!token
 
   return (
     <AuthContext.Provider
