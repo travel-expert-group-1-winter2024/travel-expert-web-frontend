af5a68d8f8949c5f2ded1484115ef44550cf21f0
CHZelaya <chzelaya1201@gmail.com>
2025-04-09 22:30:20 -0600

    [refactor] Fixed Validation on Address, removed unused validation on password field on Log in form

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -29,17 +29,7 @@ import { toast } from 'sonner'
 //Using Zod to declare a schema for the LoginForm
 const formSchema = z.object({
   email: z.string().email({ message: 'Invalid Email Address' }).trim(),
-  password: z
-    .string()
-    // .min(8, { message: 'Password must be at least 8 characters long' })
-    // .regex(/[A-Z]/, {
-    //   message: 'Password must contain at least one uppercase letter',
-    // })
-    // .regex(/[0-9]/, { message: 'Password must contain at least one number' })
-    // .regex(/[@$!%*?&]/, {
-    //   message: 'Password must contain at least one special character',
-    // })
-    .trim(),
+  password: z.string().trim(),
 })
 
 //Defining the form
