cf20de0cd9cea446b786e576311b682a41fe39a2
sorawat38 <st.sorawat38@gmail.com>
2025-04-11 01:36:46 -0600

    [refactor] update getCustomerById to use generic type for API response

--- src/api/customerApi.ts
+++ src/api/customerApi.ts
@@ -1,4 +1,5 @@
 import api from '@/api/axios.ts'
+import { Customer } from '@/types/customer.ts'
 
 export const getCustomerById = (customerId: number) =>
-  api.get(`/customers/${customerId}`)
+  api.get<Customer>(`/api/customers/${customerId}`)
