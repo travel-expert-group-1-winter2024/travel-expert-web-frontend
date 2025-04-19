45332135caf68e604fa4ab076da51d8e27d0e388
sorawat38 <st.sorawat38@gmail.com>
2025-04-18 19:36:03 -0600

    [refactor] update usePackageDetails and useRatings hooks to handle optional packageId and improve query behavior

--- src/hooks/useRatings.ts
+++ src/hooks/useRatings.ts
@@ -2,12 +2,14 @@ import { getRatings } from '@/api/ratingApi.ts'
 import { ratingsView } from '@/types/ratings'
 import { useQuery } from '@tanstack/react-query'
 
-export const useRatings = (packageId: number) => {
+export const useRatings = (packageId?: number) => {
   return useQuery<ratingsView[]>({
     queryKey: ['getAllRatings', packageId],
     queryFn: async () => {
-      const response = await getRatings(packageId)
+      const response = await getRatings(packageId!)
       return response.data
     },
+    enabled: !!packageId,
+    initialData: [],
   })
 }
