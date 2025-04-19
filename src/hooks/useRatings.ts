39bbea99279efab7feca5ade1fa54c805bfb36cb
sorawat38 <st.sorawat38@gmail.com>
2025-04-18 19:01:52 -0600

    [refactor] ratings API and refactor useRatings hook

--- src/hooks/useRatings.ts
+++ src/hooks/useRatings.ts
@@ -1,10 +1,13 @@
-import api from "@/api/axios"
-import { ratingsView } from "@/types/ratings"
-import { useQuery } from "@tanstack/react-query"
+import { getRatings } from '@/api/ratingApi.ts'
+import { ratingsView } from '@/types/ratings'
+import { useQuery } from '@tanstack/react-query'
 
 export const useRatings = (packageId: number) => {
-    return useQuery<ratingsView[]>({
-      queryKey: ['getAllRatings',packageId],
-          queryFn: () => api.get(`/api/ratings/${packageId}`).then(res => res.data),
-    })
+  return useQuery<ratingsView[]>({
+    queryKey: ['getAllRatings', packageId],
+    queryFn: async () => {
+      const response = await getRatings(packageId)
+      return response.data
+    },
+  })
 }
